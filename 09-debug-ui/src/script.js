99a7b7638712b2661d84e5dfc88731ece7c63df2
wenting yong <ywtzoe@gmail.com>
2025-12-28 16:39:37 -0500

    feat: implement debug UI with lil-gui for interactive mesh controls

--- 09-debug-ui/src/script.js
+++ 09-debug-ui/src/script.js
@@ -1,6 +1,25 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new GUI({
+    width: 400,
+    title: 'Debug UI',
+    closeFolders: false
+})
+// Hide GUI by default
+gui.hide()
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'h') {
+        gui.show(gui._hidden)
+    }
+})
+ const debugObject = {}
+
 
 /**
  * Base
@@ -14,12 +33,47 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+debugObject.color = '#a63aa2'
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
+const cubeTweaks  = gui.addFolder('Wenting Cube Tweaks')
+cubeTweaks.close()
+
+cubeTweaks.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('Position X')
+cubeTweaks.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('Position Y')
+cubeTweaks.add(mesh.position, 'z').min(-3).max(3).step(0.01).name('Position Z')
+
+cubeTweaks.add(mesh, 'visible').name('Visible')
+cubeTweaks.add(material, 'wireframe').name('Wireframe')
+cubeTweaks.addColor(debugObject, 'color').name('Color').onChange(() =>
+{
+   material.color.set(debugObject.color)
+} )
+
+debugObject.spin = () =>
+{
+    gsap.to(mesh.rotation, {duration: 1, y: mesh.rotation.y + Math.PI * 2})
+}
+cubeTweaks.add( debugObject, 'spin' ).name('Spin Cube')
+
+debugObject.subdivision = 2
+cubeTweaks.add( debugObject, 'subdivision' ).min(0).max(20).step(1).name('Subdivision')
+   // .onChange( () =>{} -> shouldn't use onchange event since can be triggeres a lot CPU wise
+    .onFinishChange( () => { 
+        //which will only be triggered when the user stops tweaking the value
+        // Create new geometry
+        mesh.geometry.dispose()//dispose the previous geometry to free memory
+        mesh.geometry = new THREE.BoxGeometry(1, 1, 1, debugObject.subdivision, debugObject.subdivision, debugObject.subdivision)
+    } )
+
+    
+
+
+/** 
  * Sizes
  */
 const sizes = {
